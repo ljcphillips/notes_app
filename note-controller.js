f18575bc94ba51b2ab1817e7c15762da7d4f6888
Lorraine Philllips <ljcphilllips@gmail.com>
2018-05-01 13:25:15 +0100

    challenge 5 complete

---
+++ note-controller.js
@@ -0,0 +1,12 @@
+// (function(window, document) {
+//   window.onload = get_app;
+//
+//   function get_app() {
+//     var app_element = document.getElementById("app");
+//     console.log(app_element);
+//   };
+// })(window,document);
+
+console.log(document.getElementById("app").innerHTML = "howdy")
+// var app = document.getElementById("app");
+// app.innerHTML = "howdy";
